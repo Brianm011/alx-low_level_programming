5e0940e8e36c247a3d6ee3072656f2ecbaf778ed
Brianm011 <brianmuriuki011@gmail.com>
2022-12-13 05:47:49 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  *
- * main - prints to the standard error
+ * main - prints exactly and that piece of art is useful
  *
- *
- *
- * Return: one
- *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
 
-		fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-			return (1);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 		              return (1);
 
 }
