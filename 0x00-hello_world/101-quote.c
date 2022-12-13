ab77b53108ce8338232addcbd007183c5a092d65
Brianm011 <brianmuriuki011@gmail.com>
2022-12-13 09:04:28 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,14 @@
-#include<stdio.h>
-
+#include <stdio.h>
 #include<unistd.h>
 
 /**
- *
- * main - Entry
- *
- * Descrition: and that piece of art is useful - Dora Korpar, 2015-10-19
- *
- *  Return: Always 1
- *
+ * main - Entry point
+ * Return: Always 1
  */
 
 int main(void)
-
 {
-     
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    
-       	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 
 }
