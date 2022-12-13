091eb4aca1aaeaeab6685a831876821eca11baad
Brianm011 <brianmuriuki011@gmail.com>
2022-12-13 04:09:40 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,11 @@
-#include <stdio.h>
-
+#include <tstdo.h>
+#include <unistd.h>
 /**
- *
- *    * main - prints to the standard error
- *
- *       *
- *
- *          * Return: one
- *
- *             */
-
+ * main - Entry
+ * Return: 1
+ */
 int main(void)
-
 {
-
-		fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-			return (1);
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59);
+	return (1);
 }
