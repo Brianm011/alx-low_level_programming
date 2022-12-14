b4992c8402dfbc636cd7ce3f4f61f798f2c26d4e
Brianm011 <brianmuriuki011@gmail.com>
2022-12-14 12:17:20 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: Always 0
+ * main - print _putchar
+ * Return: 0
  */
 int main(void)
 {
 	_putchar('_');
-	_putchar('p');
-	_putchar('u');
+	_putchar('p'); 
+        _putchar('u'); 
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef main_h
+#define man_h
+int _putchar(char c)
+
+#endif
