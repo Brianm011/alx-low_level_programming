b2309e9483de52b81a35f560d05358eee1d20aaa
Brianm011 <brianmuriuki011@gmail.com>
2022-12-15 06:13:09 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints all alphabet in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
