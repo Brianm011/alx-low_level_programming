1e2d33a1a2e6c670c7a8e24e49f22f96c8b67852
Brianm011 <brianmuriuki011@gmail.com>
2022-12-16 03:18:03 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
