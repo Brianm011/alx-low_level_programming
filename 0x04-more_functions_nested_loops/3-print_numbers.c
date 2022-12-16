03d6429976f4cc3b86fec8ad98e132b78bbdc0a9
Brianm011 <brianmuriuki011@gmail.com>
2022-12-16 07:24:14 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
