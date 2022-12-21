a431d435c75182c8e83d2717afed7e89b5f7f922
Brianm011 <brianmuriuki011@gmail.com>
2022-12-21 10:43:57 -0800

    task 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,10 +11,10 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int index = 0, src_len = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (src[index++])
+		src_len++;
 
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
