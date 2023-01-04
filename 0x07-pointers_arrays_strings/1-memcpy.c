8f60d8e6ca7676bb59fa9be691c17314cbb1d337
Brianm011 <brianmuriuki011@gmail.com>
2023-01-04 06:30:04 -0800

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memcpy - memory copy
+ * @dest: destination memory
+ * @src: source memory
+ * @n: number of bytes to be copied
+ * Return: string copied from source
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	return (dest);
+}
