c5fd4b615bbe519bc4495fd4659d8ce966946403
Brianm011 <brianmuriuki011@gmail.com>
2023-01-04 07:48:05 -0800

    task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard
+ * @a: array of pieces
+ *
+ * Return: nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
+		_putchar('\n');
+	}
+}
