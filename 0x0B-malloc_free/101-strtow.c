2f4d27e229cc0b0576c3de7a69b7c81495b16eb2
Brianm011 <brianmuriuki011@gmail.com>
2023-01-10 09:20:54 -0800

    task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
+
+/**
+ * word_len - Locates the index marking the end of the
+ *            first word contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: The index marking the end of the initial word pointed to by str.
+ */
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != ' ')
+	{
+		len++;
+		index++;
+	}
+
+	return (len);
+}
