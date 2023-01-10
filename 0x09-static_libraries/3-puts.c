e6ad3b138a8cd3ff82e2f69bde2b49ec01e0d15c
Brianm011 <brianmuriuki011@gmail.com>
2023-01-10 00:05:57 -0800

    task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_puts - print string to stdout
+ *@str: pointer parameter
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
