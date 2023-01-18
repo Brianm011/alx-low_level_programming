de8e59d2271c8060c94376b48f9b00db2bb7efe9
Brianm011 <brianmuriuki011@gmail.com>
2023-01-18 07:27:27 -0800

    task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,8 +3,8 @@
 /**
  * File: 0-object_like_macro.h
  * Desc: header file that defines a macro named
- *       SIZE as an abbreviation for the token 1024.
+ *      Size as an abbreviation for the token 1024.
  */
 
 #define SIZE 1024
-#end
+#endif
