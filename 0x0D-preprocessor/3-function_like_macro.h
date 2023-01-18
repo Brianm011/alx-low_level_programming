e89e783fe063be66685163f82eccd71cc7794207
Brianm011 <brianmuriuki011@gmail.com>
2023-01-18 07:40:57 -0800

    task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File: 3-function_like_macro.h
+ * Desc: A macro ABS(x) that returns the absolute
+ *       value of an x
+ * @x: the integer
+ */
+
+#define ABS(x) ((x) < 0 ? (x) * -1 : (x))
+
+#endif
