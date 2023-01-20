2a98516338faf7d6578ceacedd945c4962215815
Brianm011 <brianmuriuki011@gmail.com>
2023-01-20 01:05:41 -0800

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,25 +1,21 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
+
 /**
- * sum_them_all - This function returns the sum of all its parameters
- * @n: The number of arguments
- * @...: Variable arguments passed into the function
- * Return: An integer sum of all the int arguments passed into the function
+ * sum_them_all - sums of all its parameters.
+ * @n: number of parameters
+ *
+ * Return: sum of all parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
+	int sum = 0;
+	unsigned int i;
+	va_list valist;
 
-	unsigned int i, sum = 0;
-
-	if (n == 0)
-		return (0);
-	va_start(ap, n);
-
+	va_start(valist, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-
-	va_end(ap);
-
+		sum += va_arg(valist, int);
+	va_end(valist);
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,23 +1,21 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
 
 #include <stdarg.h>
-#include <stdio.h>
-
-typedef struct printer
+/**
+* struct print - print type with corresponding print function
+* @t: print type
+* @f: print function
+*/
+typedef struct print
 {
-	char *symbol;
-	void (*print)(va_list arg);
-} printer_t;
-
+	char *t;
+	void (*f)(va_list);
+} print_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
 #endif
