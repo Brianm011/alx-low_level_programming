e29edf241e651aa87ab67418f14f474f5b706c5c
Brianm011 <briankarimi11@gmail.com>
2023-02-08 07:00:35 -0800

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,27 @@
-#include "file.h"
+#include "main.h"
 
 /**
- * create_file - create a file
- * @filename: filename
- * @text_content: data
- * Return: Success ? 1 : -1
+ * create_file - function that creates a file
+ * @filename: name of file to be read
+ * @text_content: the text inside the file.
+ * Return: 1 on success, -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int file_desc, _write;
+	int file, i;
 
 	if (!filename)
 		return (-1);
 
-	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file_desc == -1)
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
-
 	if (!text_content)
-	{
-		close(file_desc);
-		return (1);
-	}
+		text_content = "";
+	for (i = 0; *(text_content + i) != '\0'; i++)
+		;
 
-	_write = write(file_desc, text_content, slen(text_content));
-	if (_write == -1 || _write != slen(text_content))
-	{
-		close(file_desc);
-		return (-1);
-	}
-	close(file_desc);
+	write(file, text_content, i);
+	close(file);
 	return (1);
 }
